eba6700a1c82de54e553178cc14d344db6c89185
sam <s.gelernter@gmail.com>
2021-11-23 17:12:34 -0500

    Complete phase II

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -3,3 +3,37 @@ Array.prototype.myEach = function(callback) {
         callback(this[i]);
     }
 }
+
+Array.prototype.myMap = function(callback) {
+    let mapped = new Array;
+    let method = function(el) {
+        mapped.push(callback(el));
+    }
+    this.myEach(method);
+    return mapped;
+}
+
+Array.prototype.myReduce = function(callback, initialValue) {
+
+    let acc = 0
+    let starterValue = true 
+
+    if (initialValue === undefined) {
+        acc = this[0];
+        starterValue = false;
+    } else {
+        acc = initialValue;
+        starterValue = true;
+    }
+    
+    let updateAcc = function(el) {
+        if (!starterValue) {
+            starterValue = true;
+        } else {
+            acc = callback(acc, el);
+        }
+    }
+
+    this.myEach(updateAcc);
+    return acc;
+}
