a57c024f16c10b45d2e8a4137cff90e7dea4d876
sam <s.gelernter@gmail.com>
2021-11-23 15:13:44 -0500

    Complete phase I

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -9,3 +9,28 @@ Array.prototype.uniq = function() {
     );
     return newArr;
 }
+
+Array.prototype.twoSum = function() {
+
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = 0; j < this.length; j++) {
+            if (j > i && this[i] + this[j] === 0) {
+                newArr.push([i, j]);    
+            }
+        }  
+    }  
+    return newArr;
+}
+
+Array.prototype.transpose = function() {
+
+   let emptyMatrix = Array.from(this[0], function() { return new Array;});
+
+   for (let i = 0; i < this.length; i++) {
+       for (let j = 0; j < emptyMatrix.length; j++) {
+           emptyMatrix[j][i] = this[i][j];
+       }
+   }
+   return emptyMatrix;
+}
